3bba8b23e0099cf28fa5b04836acbe678cbcb07e
ArcTpeEIT <arctest3903@gmail.com>
2026-07-13 09:43:44 +0800

    Add files via upload

--- tsm4_gui_factory_default_standalone.py
+++ tsm4_gui_factory_default_standalone.py
@@ -58,7 +58,8 @@ _force_utf8_stdio()
 GATEWAY_URL = "http://192.168.0.1/"
 WAIT_TIMEOUT = 30
 ROUTER_USERNAME = "admin"
-ROUTER_PASSWORD = "5nvvnaf3vr"
+#ROUTER_PASSWORD = "5nvvnaf3vr"
+ROUTER_PASSWORD = "jum8gf2zry"
 CHROME_DRIVER_PATH = str(Path(__file__).resolve().parent / "chromedriver.exe")
 
 # Primary XPath from current GUI
